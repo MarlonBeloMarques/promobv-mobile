d4a35818c2ed18e630db18c9e009ec8bdab3ee21
MarlonMarques <marlon.belohd@gmail.com>
2021-03-29 09:03:09 -0400

    fixed: linking

--- src/index.js
+++ src/index.js
@@ -13,39 +13,13 @@ export default function App() {
   const { signed, token } = useSelector((state) => state.auth, () => true);
   const [prefix, setPrefix] = useState('')
 
-  function onPrefix() {
-    if(signed) {
-      setPrefix(Linking.makeUrl('/promotions/'))
-
-      return prefix
-    } else {
-      setPrefix(Linking.makeUrl('/'))
-
-      return prefix
-    }
-  }
-
   useEffect(() => {
-    async function getUrlInitial() {
-      if(signed) {
-        let url = await Linking.getInitialURL()
-        //app desenv
-        if('details' === url.substring(27,34)) {
-          setPrefix(Linking.makeUrl('/'))
-        }
-        //app prod
-        //if('details' === url.substring(10,17)) {
-        ////  setPrefix(Linking.makeUrl('/'))
-        //}
-      }
+    async function onPrefix() {
+      setPrefix(Linking.makeUrl('/'))
     }
 
-    onPrefix()
-    getUrlInitial()
+    onPrefix();
   }, [])
-    
-  console.log(signed)
-  console.log(token)
   
   const Routes = createRoutes(signed);
 
